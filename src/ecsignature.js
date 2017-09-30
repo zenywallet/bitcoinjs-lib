09e187315de60091fb962e180e40e50334609a0f
Daniel Cousens <github@dcousens.com>
2017-10-01 00:24:26 +1000

    DER simplify

--- src/ecsignature.js
+++ src/ecsignature.js
@@ -1,52 +1,38 @@
 var bip66 = require('bip66')
-var typeforce = require('typeforce')
-var types = require('./types')
 
-var BigInteger = require('bigi')
-
-function ECSignature (r, s) {
-  typeforce(types.tuple(types.BigInt, types.BigInt), arguments)
-
-  this.r = r
-  this.s = s
+function fromDER (buffer) {
+  return bip66.decode(buffer)
 }
 
-ECSignature.fromDER = function (buffer) {
-  var decode = bip66.decode(buffer)
-  var r = BigInteger.fromDERInteger(decode.r)
-  var s = BigInteger.fromDERInteger(decode.s)
-
-  return new ECSignature(r, s)
+function toDER (rBuffer, sBuffer) {
+  return bip66.encode(rBuffer, sBuffer)
 }
 
 // BIP62: 1 byte hashType flag (only 0x01, 0x02, 0x03, 0x81, 0x82 and 0x83 are allowed)
-ECSignature.parseScriptSignature = function (buffer) {
+function fromScriptSignature (buffer) {
   var hashType = buffer.readUInt8(buffer.length - 1)
   var hashTypeMod = hashType & ~0x80
 
   if (hashTypeMod <= 0x00 || hashTypeMod >= 0x04) throw new Error('Invalid hashType ' + hashType)
 
-  return {
-    signature: ECSignature.fromDER(buffer.slice(0, -1)),
-    hashType: hashType
-  }
+  var decode = fromDER(buffer.slice(0, -1))
+  decode.hashType = hashType
+  return decode
 }
 
-ECSignature.prototype.toDER = function () {
-  var r = Buffer.from(this.r.toDERInteger())
-  var s = Buffer.from(this.s.toDERInteger())
-
-  return bip66.encode(r, s)
-}
-
-ECSignature.prototype.toScriptSignature = function (hashType) {
+function toScriptSignature (signature, hashType) {
   var hashTypeMod = hashType & ~0x80
   if (hashTypeMod <= 0 || hashTypeMod >= 4) throw new Error('Invalid hashType ' + hashType)
 
   var hashTypeBuffer = Buffer.alloc(1)
   hashTypeBuffer.writeUInt8(hashType, 0)
 
-  return Buffer.concat([this.toDER(), hashTypeBuffer])
+  return Buffer.concat([toDER(signature), hashTypeBuffer])
 }
 
-module.exports = ECSignature
+module.exports = {
+  fromDER,
+  toDER,
+  fromScriptSignature,
+  toScriptSignature
+}
