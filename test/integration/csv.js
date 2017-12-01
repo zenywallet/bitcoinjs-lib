0806c61a44e0fa3d0096062759d665abf1b1e91c
Daniel Cousens <github@dcousens.com>
2017-12-01 13:23:31 +1100

    add BIP68 and BIP112 test integration example

--- test/integration/csv.js
+++ test/integration/csv.js
@@ -3,45 +3,32 @@
 var assert = require('assert')
 var bitcoin = require('../../')
 var testnetUtils = require('./_testnet')
+var bip68 = require('bip68')
 
 var testnet = bitcoin.networks.testnet
 var alice = bitcoin.ECPair.fromWIF('cScfkGjbzzoeewVWmU2hYPUHeVGJRDdFt7WhmrVVGkxpmPP8BHWe', testnet)
-var bob = bitcoin.ECPair.fromWIF('cMkopUXKWsEzAjfa1zApksGRwjVpJRB3831qM9W4gKZsLwjHXA9x', testnet)
 
 describe('bitcoinjs-lib (transactions w/ CSV)', function () {
   var hashType = bitcoin.Transaction.SIGHASH_ALL
 
-  // IF MTP > utcSeconds, aQ can redeem, ELSE bQ, aQ joint redeem
-  function cltvCheckSigOutput (aQ, bQ, utcSeconds) {
+  // IF MTP (from when confirmed) > seconds, aQ can redeem
+  function p2pkhCSV (sequence) {
     return bitcoin.script.compile([
-      [height] CSV DROP DUP HASH160 [hash] EQUALVERIFY CHECKSIG
-
-      bitcoin.opcodes.OP_IF,
-      bitcoin.script.number.encode(utcSeconds),
+      bitcoin.script.number.encode(sequence),
       bitcoin.opcodes.OP_CHECKSEQUENCEVERIFY,
       bitcoin.opcodes.OP_DROP,
-
-      bitcoin.opcodes.OP_ELSE,
-      bQ.getPublicKeyBuffer(),
-      bitcoin.opcodes.OP_CHECKSIGVERIFY,
-      bitcoin.opcodes.OP_ENDIF,
-
-      aQ.getPublicKeyBuffer(),
+      alice.getPublicKeyBuffer(),
       bitcoin.opcodes.OP_CHECKSIG
     ])
   }
 
-  function utcNow () {
-    return Math.floor(Date.now() / 1000)
-  }
-
-  // expiry past, {Alice's signature} OP_TRUE
-  it('can create (and broadcast via 3PBP) a Transaction where Alice can redeem the output after the expiry', function (done) {
+  it('can create (and fail to broadcast via 3PBP) a Transaction where Alice wants to redeem, but must wait for an expiry', function (done) {
     this.timeout(30000)
 
-    // three hours ago
-    var timeUtc = utcNow() - (3600 * 3)
-    var redeemScript = cltvCheckSigOutput(alice, bob, timeUtc)
+    // 1 hour from now
+    var sequence = bip68.encode({ seconds: 3600 })
+
+    var redeemScript = p2pkhCSV(sequence)
     var scriptPubKey = bitcoin.script.scriptHash.output.encode(bitcoin.crypto.hash160(redeemScript))
     var address = bitcoin.address.fromOutputScript(scriptPubKey, testnet)
 
@@ -50,21 +37,20 @@ describe('bitcoinjs-lib (transactions w/ CSV)', function () {
       if (err) return done(err)
 
       var tx = new bitcoin.TransactionBuilder(testnet)
-      tx.setLockTime(timeUtc)
-      tx.addInput(unspent.txId, 0, 0xfffffffe)
+      tx.addInput(unspent.txId, 0, sequence)
       tx.addOutput(testnetUtils.RETURN_ADDRESS, 1e4)
 
       var txRaw = tx.buildIncomplete()
       var signatureHash = txRaw.hashForSignature(0, redeemScript, hashType)
 
-      // {Alice's signature} OP_TRUE
+      // {Alice's signature}
       var redeemScriptSig = bitcoin.script.scriptHash.input.encode([
-        alice.sign(signatureHash).toScriptSignature(hashType),
-        bitcoin.opcodes.OP_TRUE
+        alice.sign(signatureHash).toScriptSignature(hashType)
       ], redeemScript)
 
       txRaw.setInputScript(0, redeemScriptSig)
 
+      // TODO: fix
       testnetUtils.transactions.propagate(txRaw.toHex(), done)
     })
   })
